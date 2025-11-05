122621ab35676bceb6df237391bb66d2d415d7b5
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-05 15:46:58 +0530

    A02: Initial commit

---
+++ A02/Program.cs
@@ -0,0 +1,19 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to implement a simple guessing game
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+
+int num = new Random ().Next (1, 101);
+while (true) {
+   Write ("Enter your guess (1 to 100): ");
+   if (int.TryParse (ReadLine (), out int guess) && guess >= 1 && guess <= 100) {
+      if (guess == num) {
+         WriteLine ("You guessed correctly"); break;
+      }
+      WriteLine ("Your guess is too " + (guess > num ? "high" : "low"));
+   } else WriteLine ("Invalid input!");
+}
