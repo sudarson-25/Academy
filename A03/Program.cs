76caa232e76925de323a29ed36a7b8826f7679b3
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-10 11:51:46 +0530

    Review fix

--- A03/Program.cs
+++ A03/Program.cs
@@ -13,27 +13,23 @@ class Program {
    static void Main () {
       int total = 0;
       string[] words = File.ReadAllLines ("C:/etc/words.txt");
-      foreach (var (Score, Word, Pangram) in words.Where (IsValid).Select (GetScore)
-                                                  .OrderByDescending (a => a.Score)
-                                                  .ThenBy (a => a.Word)) {
-         if (Pangram) ForegroundColor = ConsoleColor.Green;
-         WriteLine ($"{Score,3}. {Word}");
+      var results = words.Where (IsValid).Select (GetScore).OrderByDescending (a => a.Score)
+         .ThenBy (a => a.Word);
+      foreach (var result in results) {
+         if (result.IsPangram) ForegroundColor = ConsoleColor.Green;
+         WriteLine ($"{result.Score,3}. {result.Word}");
          ResetColor ();
-         total += Score;
+         total += result.Score;
       }
       WriteLine ($"----\n{total,3} total");
    }
 
-   static bool IsValid (string word) {
-      if (word.Length >= 4 && word.Contains (sLetters[0]) &&
-         word.All (sLetters.Contains)) return true;
-      return false;
-   }
+   static bool IsValid (string word) => word.Length >= 4 && word.Contains ('U') &&
+         word.All (sLetters.Contains);
 
-   static (int Score, string Word, bool Pangram) GetScore (string word) {
+   static (int Score, string Word, bool IsPangram) GetScore (string word) {
       bool pangram = sLetters.All (word.Contains);
-      int score = (word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0);
-      return (score, word, pangram);
+      return ((word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0), word, pangram);
    }
 
    static char[] sLetters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
