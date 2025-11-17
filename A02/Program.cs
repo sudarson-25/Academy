3e6f91ac9d572ec5db870f1413086e78045eab71
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-17 14:58:27 +0530

    Review fix 2

--- A02/Program.cs
+++ A02/Program.cs
@@ -8,11 +8,12 @@
 using static System.Console;
 
 const int MIN = 1, MAX = 100;
-int num = new Random ().Next (MIN, MAX + 1);
-while (true) {
+int num = new Random ().Next (MIN, MAX + 1), i;
+for (i = 0; i < 7; i++) {
    Write ($"Enter your guess ({MIN} to {MAX}): ");
    if (int.TryParse (ReadLine (), out int guess) && guess >= MIN && guess <= MAX) {
       if (guess == num) { WriteLine ("You guessed correctly"); break; }
       WriteLine ($"Your guess is too {(guess > num ? "high" : "low")}");
    } else WriteLine ("Invalid input!");
 }
+if (i == 7) WriteLine ("Maximum attempts reached!");
