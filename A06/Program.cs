a3bbe7a8fc92d1a4d349f9069366cd56646c1a5d
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-17 14:47:15 +0530

    Review fix

--- A06/Program.cs
+++ A06/Program.cs
@@ -5,23 +5,34 @@
 // Program.cs
 // Program to solve the 8 queens problem
 // ------------------------------------------------------------------------------------------------
-using System.Text;
 using static System.Console;
 namespace A06;
 
 class Program {
    static void Main () {
-      OutputEncoding = new UnicodeEncoding ();
-      PlaceQueen (0);
-      PrintSolutions (sSolns);
+      while (true) {
+         Write ("Menu\n~~~~\n1.Print all the solutions\n2.Print unique solutions" +
+            "\nEnter your choice: ");
+         if (!int.TryParse (ReadLine (), out int choice) || (choice != 1 && choice != 2)) {
+            WriteLine ("Invalid input!\n"); continue;
+         }
+         sSolns.Clear ();
+         if (choice == 1) PlaceQueen (0, false);
+         else PlaceQueen (0, true);
+         PrintSolutions (sSolns);
+         WriteLine ("Do you want to continue (y/n)?");
+         if (ReadKey (true).Key is not ConsoleKey.Y) break;
+         Clear ();
+      }
    }
 
    // Adds a valid solution to the list of solutions.
-   static void AddSolution (int[] soln) {
-      for (int i = 0; i < 4; i++) {
-         soln = Rotate (soln);
-         if (Exists (soln) || Exists (VMirror (soln))) return;
-      }
+   static void AddSolution (int[] soln, bool unique) {
+      if (unique)
+         for (int i = 0; i < 4; i++) {
+            soln = Rotate (soln);
+            if (Exists (soln) || Exists (VMirror (soln))) return;
+         }
       sSolns.Add (soln);
 
       // Checks if the solution exists.
@@ -39,17 +50,17 @@ class Program {
    }
 
    // Recursive function to place queens on the chessboard.
-   static void PlaceQueen (int row) {
+   static void PlaceQueen (int row, bool unique) {
       for (sPos[row] = 0; sPos[row] < N; sPos[row]++)
          if (IsValid ())
-            if (row == N - 1) AddSolution ([.. sPos]);
-            else PlaceQueen (row + 1);
+            if (row == N - 1) AddSolution ([.. sPos], unique);
+            else PlaceQueen (row + 1, unique);
 
       // Checks if the current position of the queen is valid.
       bool IsValid () {
          for (int r = 0; r < row; r++) {
-            int dy = row - r, dx = Math.Abs (sPos[row] - sPos[r]);
-            if (dx == 0 || dx == dy) return false;
+            int dx = Math.Abs (sPos[row] - sPos[r]);
+            if (dx == 0 || dx == row - r) return false;
          }
          return true;
       }
@@ -57,16 +68,19 @@ class Program {
 
    // Prints all the valid solutions of queens on the chessboard.
    static void PrintSolutions (List<int[]> solutions) {
-      int count = 1;
-      foreach (var solution in solutions) {
-         WriteLine ($"Solution: {count++}\n┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓");
+      Clear ();
+      OutputEncoding = new System.Text.UnicodeEncoding ();
+      for (int i = 0; i < solutions.Count; i++) {
+         CursorLeft = CursorTop = 0;
+         WriteLine ($"Solution: {i + 1}\n┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓");
          for (int row = 0; row < N; row++) {
             Write ("┃");
             for (int col = 0; col < N; col++)
-               Write (solution[col] == row ? $" ♛  ┃" : $"    ┃");
+               Write (solutions[i][col] == row ? $" ♛  ┃" : $"    ┃");
             if (row < N - 1) WriteLine ("\n┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫");
          }
          WriteLine ("\n┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛");
+         ReadKey ();
       }
    }
 
