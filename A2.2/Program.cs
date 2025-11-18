ae6db5c2ec99c7972546a83b3c9498b8dc97c18c
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-18 14:06:28 +0530

    Review fix 2

--- A2.2/Program.cs
+++ A2.2/Program.cs
@@ -11,36 +11,39 @@ namespace A2._2;
 
 class Program {
    static void Main () {
-      const int MIN = 1, MAX = 100;
+      const int MIN = 0, MAX = 255;
+      int secretNum;
       while (true) {
          Write ($"Enter a secret number ({MIN} to {MAX}): ");
-         if (int.TryParse (ReadLine (), out int secretNum) && secretNum >= MIN && secretNum <= MAX)
+         if (int.TryParse (ReadLine (), out secretNum) && secretNum >= MIN && secretNum <= MAX)
             break;
          WriteLine ("Invalid number!");
       }
-      WriteLine ($"\nSecret number: {GetSecretNum (out char[] revBin)}\nLSB->" +
-         $"{new string (revBin)}<-MSB");
+      if (IsNumberFound (secretNum, out char[] revBin))
+         WriteLine ($"\nSecret number: {secretNum}\nLSB->{new string (revBin)}<-MSB");
+      else WriteLine ("\nIncorrect answers!");
    }
 
-   // Returns the secret number by processing bits from least to most significant
-   static int GetSecretNum (out char[] revBin) {
-      int secretNum = 0;
+   // Checks if the number found by querying the user for each bit matches the secret number.
+   static bool IsNumberFound (int secretNum, out char[] revBin) {
+      int num = 0;
       revBin = new char[8];
       for (int i = 0; i < 8; i++) {
          ConsoleKey input;
          while (true) {
             // 1<<i gives the value of the current bit position
-            Write ($"\nIs the remainder >= {1 << i} when the secret number is divided by " +
-               $"{1 << (i + 1)}? (Y/N) ");// 1<<(i+1) is the divisor to isolate that bit
+            Write ($"\nIs the remainder >= {1 << i} when the secret number is divided by ");
+            Write ($"{1 << (i + 1)}? (Y/N) ");// 1<<(i+1) is the divisor to isolate that bit
             input = ReadKey ().Key;
             if (input is ConsoleKey.Y or ConsoleKey.N) break;
             Write ("\nInvalid input");
          }
          if (input == ConsoleKey.Y) {
-            secretNum |= 1 << i;
+            num |= 1 << i;
             revBin[i] = '1';
          } else revBin[i] = '0';
       }
-      return secretNum;
+      if (num == secretNum) return true;
+      return false;
    }
 }
