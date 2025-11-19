fe501873efaeb2dd87430ded18e4cc176d2b78de
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-19 17:36:15 +0530

    A04: Spelling Bee Extension (#5)

---
+++ A04/Program.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to build a frequency table with occurrence of the 7 most common letters
+// ------------------------------------------------------------------------------------------------
+namespace A04;
+
+class Program {
+   static void Main () {
+      var frequencyTable = File.ReadAllText ("c:/etc/words.txt").Where (char.IsLetter)
+         .GroupBy (char.ToUpper).OrderByDescending (g => g.Count ()).Take (7);
+      foreach (var group in frequencyTable) Console.WriteLine ($"{group.Key} {group.Count ()}");
+   }
+}
