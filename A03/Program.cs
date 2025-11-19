21c6f2a249f3be773d60427863c7eaf448e0f55d
Sudarson <sudarson.selvakumar@trumpf.com>
2025-11-19 11:24:18 +0530

    A03: Spelling Bee (#4)

---
+++ A03/Program.cs
@@ -0,0 +1,37 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to find all valid Spelling Bee words form the given dictionary
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+
+namespace A03;
+
+class Program {
+   static void Main () {
+      int total = 0;
+      var results = File.ReadAllLines ("C:/etc/words.txt").Select (word => word.Trim ().ToUpper ())
+         .Where (IsValid).Select (GetScore).OrderByDescending (a => a.Score).ThenBy (a => a.Word);
+      foreach (var (Score, Word, IsPangram) in results) {
+         if (IsPangram) ForegroundColor = ConsoleColor.Green;
+         WriteLine ($"{Score, 3}. {Word}");
+         ResetColor ();
+         total += Score;
+      }
+      WriteLine ($"----\n{total, 3} total");
+   }
+
+   // Returns whether the given word is valid
+   static bool IsValid (string word) =>
+      word.Length >= 4 && word.Contains ('U') && word.All (sLetters.Contains);
+
+   // Return the score, word and whether it is a pangram
+   static (int Score, string Word, bool IsPangram) GetScore (string word) {
+      bool pangram = sLetters.All (word.Contains);
+      return ((word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0), word, pangram);
+   }
+
+   static char[] sLetters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
+}
