01e4fab2d63c76d6c5f02bc663557b80fecac408
Sudarson <sudarson.selvakumar@trumpf.com>
2025-12-16 12:45:10 +0530

    Initial commit

---
+++ A9.1/Token.cs
@@ -0,0 +1,99 @@
+﻿namespace A9._1;
+
+abstract class Token { }
+
+abstract class TNumber : Token {
+   public abstract double Value { get; }
+}
+
+class TLiteral (double f) : TNumber {
+   public override double Value => mValue;
+   public override string ToString () => $"literal:{Value}";
+   readonly double mValue = f;
+}
+
+class TVariable : TNumber {
+   public TVariable (Evaluator eval, string name) => (Name, mEval) = (name, eval);
+   public string Name { get; private set; }
+   public override double Value => mEval.GetVariable (Name);
+   public override string ToString () => $"var:{Name}";
+   readonly Evaluator mEval;
+}
+
+abstract class TOperator (Evaluator eval) : Token {
+   public int Priority { get; protected set; }
+   readonly protected Evaluator mEval = eval;
+}
+
+class TOpArithmetic : TOperator {
+   public TOpArithmetic (Evaluator eval, char ch) : base (eval) {
+      Op = ch;
+      Priority = sPriority[Op] + mEval.BasePriority;
+   }
+   public char Op { get; set; }
+   public override string ToString () => $"op:{Op}:{Priority}";
+   static Dictionary<char, int> sPriority = new () {
+      ['+'] = 1, ['-'] = 1, ['*'] = 2, ['/'] = 2, ['^'] = 3, ['='] = 4,
+   };
+   public double Evaluate (double a, double b) {
+      return Op switch {
+         '+' => a + b,
+         '-' => a - b,
+         '*' => a * b,
+         '/' => a / b,
+         '^' => Math.Pow (a, b),
+         _ => throw new EvalException ($"Unknown operator: {Op}"),
+      };
+   }
+}
+
+class TOpUnary : TOperator {
+   public TOpUnary (Evaluator eval, char ch) : base (eval) {
+      Op = ch;
+      Priority = 5 + mEval.BasePriority;
+   }
+   public char Op { get; private set; }
+   public override string ToString () => $"unaryOp:{Op}:{Priority}";
+
+   public double Evaluate (double f) => Op == '+' ? f : -f;
+}
+
+class TOpFunction : TOperator {
+   public TOpFunction (Evaluator eval, string name) : base (eval) {
+      Func = name;
+      Priority = 5 + mEval.BasePriority;
+   }
+   public string Func { get; private set; }
+   public override string ToString () => $"func:{Func}:{Priority}";
+   public double Evaluate (double f) {
+      return Func switch {
+         "sin" => Math.Sin (D2R (f)),
+         "cos" => Math.Cos (D2R (f)),
+         "tan" => Math.Tan (D2R (f)),
+         "sqrt" => Math.Sqrt (f),
+         "log" => Math.Log (f),
+         "exp" => Math.Exp (f),
+         "asin" => R2D (Math.Asin (f)),
+         "acos" => R2D (Math.Acos (f)),
+         "atan" => R2D (Math.Atan (f)),
+         _ => throw new EvalException ($"Unknown function: {Func}")
+      };
+
+      double D2R (double f) => f * Math.PI / 180;
+      double R2D (double f) => f * 180 / Math.PI;
+   }
+}
+
+class TPunctuation (char ch) : Token {
+   public char Punct { get; private set; } = ch;
+   public override string ToString () => $"punct:{Punct}";
+}
+
+class TEnd : Token {
+   public override string ToString () => "end";
+}
+
+class TError (string message) : Token {
+   public string Message { get; private set; } = message;
+   public override string ToString () => $"error:{Message}";
+}
---
+++ A9.1/Tokenizer.cs
@@ -0,0 +1,59 @@
+﻿namespace A9._1;
+
+class Tokenizer (Evaluator eval, string text) {
+   public Token Next () {
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         Token? prevToken;
+         switch (ch) {
+            case ' ' or '\t': continue;
+            case (>= '0' and <= '9') or '.':
+               prevToken = (mPrev is TLiteral) ? new TError ("Invalid Operation") : GetNumber ();
+               break;
+            case '(' or ')':
+               mEval.BasePriority += ch == '(' ? 10 : -10;
+               prevToken = new TPunctuation (ch); break;
+            case '+' or '-':
+               prevToken = (mPrev is null || mPrev is TOperator or TPunctuation { Punct: '(' })
+                   ? new TOpUnary (mEval, ch) : new TOpArithmetic (mEval, ch); break;
+            case '*' or '/' or '^' or '=': prevToken = new TOpArithmetic (mEval, ch); break;
+            case >= 'a' and <= 'z': prevToken = GetIdentifier (); break;
+            default: prevToken = new TError ($"Unknown symbol: {ch}"); break;
+         }
+         mPrev = prevToken;
+         return prevToken;
+      }
+      return new TEnd ();
+   }
+
+   Token GetIdentifier () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = char.ToLower (mText[mN++]);
+         if (ch is >= 'a' and <= 'z') continue;
+         mN--; break;
+      }
+      string sub = mText[start..mN];
+      if (mFuncs.Contains (sub)) return new TOpFunction (mEval, sub);
+      else return new TVariable (mEval, sub);
+   }
+   readonly string[] mFuncs = { "sin", "cos", "tan", "sqrt", "log", "exp", "asin", "acos", "atan" };
+
+   Token GetNumber () {
+      int start = mN - 1;
+      while (mN < mText.Length) {
+         char ch = mText[mN++];
+         if (ch is >= '0' and <= '9' or '.') continue;
+         mN--; break;
+      }
+      // Now, mN points to the first character of mText that is not part of the number
+      string sub = mText[start..mN];
+      if (double.TryParse (sub, out double f)) return new TLiteral (f);
+      return new TError ($"Invalid number: {sub}");
+   }
+
+   Token? mPrev;
+   int mN = 0;                    // Position within the text
+   readonly Evaluator mEval = eval;  // The evaluator that owns this
+   readonly string mText = text;     // The input text we're parsing through
+}
