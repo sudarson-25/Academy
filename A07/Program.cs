262aaf62fa577f5902a404a1578560f334897706
Sudarson <sudarson.selvakumar@trumpf.com>
2025-12-17 15:07:58 +0530

    Review fix

--- A07/Program.cs
+++ A07/Program.cs
@@ -12,89 +12,80 @@ namespace A07;
 class Program {
    static void Main () {
       Write ($"Enter the string to be parsed: ");
-      WriteLine (TryParse (ReadLine () ?? "".Trim ()
-         .ToLower (), out double result) ? $"Parsed value: {result}" : "Invalid input!");
+      WriteLine (TryParse ((ReadLine () ?? "").Trim ().ToLower (), out double result) ?
+         $"Parsed value: {result}" : "Invalid input!");
    }
 
    // Tries to parse string value into double
    static bool TryParse (string input, out double result) {
       result = double.NaN;
-      if (input == "") return false;
+      if (input.Length == 0) return false;
       int count = 0;
       // Filters those inputs having successive signs
       while ((input[count] is '+' or '-') && (count < input.Length)) {
          count++;
          if (count > 1) return false;
       }
-      bool isNegative = input[0] == '-';
-      if (input[0] == '+' || isNegative) input = input[1..];
-      int eIndex = input.IndexOfAny (['e', 'E']);
+      input = GetAbsoluteValue (input, out bool isNegative);
+      if (input.Length == 0) return false;
+      int eIndex = input.IndexOf ('e');
       bool hasIndex = eIndex > 0;
       string basePart = hasIndex ? input[..eIndex] : input,
          expPart = hasIndex ? input[(eIndex + 1)..] : "0";
-      bool isBaseParsed = TryParseBase (basePart, out double b),
-         isExpParsed = TryParseExp (expPart, out double e);
-      if (isBaseParsed && isExpParsed) result = b * Math.Pow (10, e) * (isNegative ? -1 : 1);
-      else return false;
+      if (basePart.StartsWith ('.') || basePart.EndsWith ('.')) return false;
+      int i = 0, dotIndex = -1;
+      while (i < basePart.Length) {
+         char ch = basePart[i];
+         if (ch == '.') {
+            if (dotIndex != -1) return false;
+            dotIndex = i++;
+            continue;
+         }
+         if (ch < '0' || ch > '9') return false;
+         i++;
+      }
+      if (expPart.Length == 0) return false;
+      expPart = GetAbsoluteValue (expPart, out bool isExpNegative);
+      if (expPart.Length == 0) return false;
+      int j = 0;
+      while (j < expPart.Length) {
+         char ch = expPart[j++];
+         if (ch < '0' || ch > '9') return false;
+      }
+      double baseValue = GetBaseValue (basePart, dotIndex);
+      int expValue = GetExpValue (expPart, isExpNegative);
+      result = baseValue * Math.Pow (10, expValue) * (isNegative ? -1 : 1);
       return true;
    }
 
    // Tries to parse string base value into double
-   static bool TryParseBase (string basePart, out double b) {
-      if (basePart.StartsWith ('.') || basePart.EndsWith ('.')) {
-         b = double.NaN;
-         return false;
-      }
-      bool hasDecimal = false;
+   static double GetBaseValue (string basePart, int dotIndex) {
       int i = 0;
-      double decimalFactor = 0.1;
-      b = 0.0;
+      double decimalFactor = 0.1, b = 0.0;
       while (i < basePart.Length) {
-         char ch = basePart[i];
-         if (ch == '.') {
-            if (hasDecimal) {
-               b = double.NaN;
-               return false;
-            }
-            hasDecimal = true;
-            i++;
-            continue;
+         int digit = basePart[i] - '0';
+         if (i == dotIndex) { i++; continue; }
+         if (dotIndex == -1 || i < dotIndex) b = b * 10 + digit;
+         else {
+            b += digit * decimalFactor;
+            decimalFactor /= 10;
          }
-         if (ch < '0' || ch > '9') {
-            b = double.NaN;
-            return false;
-         }
-         int digit = ch - '0';
-         if (!hasDecimal) b = b * 10 + digit;
-         else { b += digit * decimalFactor; decimalFactor /= 10; }
          i++;
       }
-      return true;
+      return b;
    }
 
    // Tries to parse string exponent value into double
-   static bool TryParseExp (string expPart, out double e) {
-      if (expPart.Length == 0) {
-         e = double.NaN;
-         return false;
-      }
-      int i = 0;
-      bool isNegative = expPart[i] == '-';
-      if (expPart[i] == '+' || isNegative) i++;
-      if (i == expPart.Length) {
-         e = double.NaN;
-         return false;
-      }
-      e = 0;
-      while (i < expPart.Length) {
-         char ch = expPart[i++];
-         if (ch < '0' || ch > '9') {
-            e = double.NaN;
-            return false;
-         }
-         e = e * 10 + (ch - '0');
-      }
-      if (isNegative) e *= -1;
-      return true;
+   static int GetExpValue (string expPart, bool isExpNegative) {
+      int e = 0;
+      foreach (char ch in expPart) e = e * 10 + (ch - '0');
+      if (isExpNegative) e = -e;
+      return e;
+   }
+
+   static string GetAbsoluteValue (string input, out bool isNegative) {
+      isNegative = input[0] == '-';
+      if (input[0] == '+' || isNegative) return input[1..];
+      return input;
    }
 }
