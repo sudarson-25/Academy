4614687ca8757da9ad87a8ba2c5691c7ce64fd77
Sudarson <sudarson.selvakumar@trumpf.com>
2025-12-29 13:09:40 +0530

    T1: Initial commit

---
+++ T1/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+namespace T1;
+
+class Program {
+   static void Main () {
+      while (true) {
+         Write ("Input: ");
+         string input;
+         do input = ReadLine ()!; while (input == null);
+         DoAction (input.Trim (), commands);
+      }
+   }
+
+   static void DoAction (string input, Stack<(char, string)> commands) {
+      if (input.Length > 3 && input[..3] is "ADD") {
+         commands.Push (('A', input[3..]));
+         text += input[3..].Trim ();
+         return;
+      }
+      if (input.Length > 6 && input[..6] is "DELETE") {
+         if (!int.TryParse (input[6..].Trim (), out int number) || number > text.Length) return;
+         commands.Push (('D', text[(text.Length - number)..])); //Pushing deleted text
+         text = text[..(text.Length - number)];
+         return;
+      }
+      if (input is "SHOW") Console.WriteLine (text);
+      if (input is "EXIT") return;
+      if (input is "UNDO") {
+         if (commands is not null) {
+            (char command, string content) = commands.Pop ();
+            if (command is 'A') DoAction ("DELETE" + content.Length, undo);
+            if (command is 'D') DoAction ("ADD" + content, undo);
+         }
+         return;
+      }
+      if (input is "REDO") {
+         if (undo is not null) {
+            (char command, string content) = undo.Pop ();
+            if (command is 'A') DoAction ("DELETE" + content.Length, commands);
+            if (command is 'D') DoAction ("ADD" + content, commands);
+         }
+         return;
+      }
+   }
+
+   static string text = "";
+   static Stack<(char, string)> commands = new (), undo = new ();
+}
